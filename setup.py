31322a0220af49e9a35598a93d6801589b4b6356
hwrn <hwrn.aou@sjtu.edu.cn>
2025-01-10 21:45:59 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from distutils.core import setup
+if __name__ == "__main__":
+    from setuptools import setup
 
-setup(
-    name='genomediff2',
-    version='0.3.3',
-    packages=['genomediff'],
-    url='https://github.com/biosustain/genomediff-python.git',
-    license='MIT',
-    author='Lars Schoening',
-    author_email='lays@biosustain.dtu.dk',
-    description='GenomeDiff (*.gd) file reader',
-    long_description='GenomeDiff file reader',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Other Environment',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-    ]
-)
+    setup(
+        name="genomediff",
+        version="0.4.0",
+        packages=["genomediff"],
+        url="https://github.com/Hocnonsense/genomediff-python",
+        license="MIT",
+        author="Aoran Hu",
+        author_email="hwrn.aou@sjtu.edu.cn",
+        description="GenomeDiff (*.gd) file reader",
+        long_description="GenomeDiff file reader",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Other Environment",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering :: Bio-Informatics",
+        ],
+    )
